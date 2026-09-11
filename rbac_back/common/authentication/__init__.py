14d91dc50f37fad5d367c34733262e18aab2157b
61ackPink <v61ackPink@163.com>
2026-09-11 16:20:05 +0800

    JWT 身份认证类

---
+++ rbac_back/common/authentication/__init__.py
@@ -0,0 +1,11 @@
+"""
+-*- coding: utf-8 -*-
+@File  : __init__.py
+@Author: 61ackPink
+@Time : 2026/9/11 16:07
+@Desc : 
+"""
+from .jwt_authentication import JWTAuthentication
+
+
+__all__ = ["JWTAuthentication"]
