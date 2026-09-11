b178dea4205c6426dfbcde0af09074ad7f00a553
61ackPink <v61ackPink@163.com>
2026-09-11 16:39:16 +0800

    重写json render

--- rbac_back/common/custom/json_render.py
+++ rbac_back/common/custom/json_render.py
@@ -9,15 +9,47 @@ from rest_framework.renderers import JSONRenderer
 
 class Renderer(JSONRenderer):
     """重写Json渲染器"""
-    def render(self, data ,media_type=None, renderer_context=None):
-        if renderer_context:
-            # 自定义返回
-            response = {
-                'code': renderer_context['response'].status_code,
-                'message': 'success',
-                'data': data
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        # 没有响应上下文时，使用 DRF 默认处理
+        if not renderer_context:
+            return super().render(
+                data,
+                accepted_media_type,
+                renderer_context,
+            )
+        response = renderer_context["response"]
+        status_code = response.status_code
+        is_success = status_code < 400
+        if is_success:
+            result = {
+                "code": status_code,
+                "message": "success",
+                "data": data,
             }
-            # 返回json格式
-            return super().render(response, media_type, renderer_context)
         else:
-            return super().render(data, media_type, renderer_context)
+            result = {
+                "code": status_code,
+                "message": "error",
+                "data": None,
+                "errors": data,
+            }
+
+        return super().render(
+            result,
+            accepted_media_type,
+            renderer_context,
+        )
+
+
+    # def render(self, data ,media_type=None, renderer_context=None):
+    #     if renderer_context:
+    #         # 自定义返回
+    #         response = {
+    #             'code': renderer_context['response'].status_code,
+    #             'message': 'success',
+    #             'data': data
+    #         }
+    #         # 返回json格式
+    #         return super().render(response, media_type, renderer_context)
+    #     else:
+    #         return super().render(data, media_type, renderer_context)
