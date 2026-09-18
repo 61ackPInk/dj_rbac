072b916e9357a66aef832dde2b0cda1529631a24
61ackPink <v61ackPink@163.com>
2026-09-18 18:24:09 +0800

    网站公共色值

--- rbac_front/src/views/auth/login/login.vue
+++ rbac_front/src/views/auth/login/login.vue
@@ -2,25 +2,82 @@
 <script src="./login.js"></script>
 
 <template>
-    <main class="login-page">
-        <!-- 左侧面板 -->
-        <div class="left-panel">
-            <i class="bi bi-gift"></i>
-            
+    <div class="login-warp">
+        <!-- 左侧内容，移动端隐藏 -->
+        <div class="login-left">
+            <div class="left contetnt">
+                <h1>WELCOME BACK</h1>
+                <p>Please sign in to continue.</p>
+            </div>
         </div>
-        <!-- 右侧面板 -->
-        <div class="right-panel">
 
+        <!-- 右侧内容 登录-->
+        <div class="login-right">
+            <div class="login-card">
+                <h2 class="title">账户登录</h2>
+                <div class="sub-desc">请输入你的账号信息登录系统</div>
+
+                <!-- 账号 -->
+                <div class="form-item">
+                    <div class="input-wrap">
+                        <i class="bi bi-person"></i>
+                        <input class="username-input" type="text" placeholder="账号">
+                    </div>
+                </div>
+
+                <!-- 密码 -->
+                <div class="form-item">
+                    <div class="input-wrap">
+                        <i class="bi bi-lock"></i>
+                        <input class="password-input" type="password" placeholder="密码">
+                    </div>
+                </div>
+
+                <!-- 记住密码 + 忘记密码 -->
+                <div class="form-row-between">
+                    <label>
+                        <input type="checkbox">
+                        记住密码
+                    </label>
+                    <a href="#">忘记密码？</a>
+                </div>
+
+                <!-- 登录主按钮 -->
+                <div class="form-item">
+                    <button class="apple-btn-solid">
+                        <i class="bi bi-box-arrow-in-right"></i>
+                        登录
+                    </button>
+                </div>
+
+                <!-- 分割线 -->
+                <div class="divider">
+                    <span>或使用以下方式登录</span>
+                </div>
+
+                <!-- 第三方登录按钮组 -->
+                <div class="form-item social-login">
+                    <button class="social-btn">
+                        <i class="bi bi-github"></i>
+                        Github
+                    </button>
+                    <button class="social-btn">
+                        <i class="bi bi-wechat"></i>
+                        微信
+                    </button>
+                </div>
+
+                <!-- 注册跳转提示 -->
+                <div class="tip-text">
+                    还没有账号？ <a href="#" class="link-text">立即注册</a>
+                </div>
+            </div>
         </div>
-    </main>
+
+    </div>
 </template>
 
 <!-- scoped 保证样式仅作用于当前页面 -->
 <style scoped lang="scss">
-    .login-page {
-        display: flex;
-        height: 100vh;
-        width: 100vw;
-        background-color: var(--app-success);
-    }
+
 </style>
