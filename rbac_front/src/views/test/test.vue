bc1456c87f35e1f04a9d470f3d4894f84c9ad36c
61ackPink <v61ackPink@163.com>
2026-09-23 18:29:52 +0800

    在test重写登录页

--- rbac_front/src/views/test/test.vue
+++ rbac_front/src/views/test/test.vue
@@ -1,15 +1,41 @@
+<script>
+
+</script>
+
 <template>
+  <!-- 登陆页面 -->
   <div class="login-warp">
+    <!-- 左侧 -->
+    <div class="login-left">
+        <h1 class="title">>WELCOME BACK</h1>
+    </div>
+    <!-- 右侧 -->
+    <div class="login-right">
+      <div class="login-card">
+        <h2 class="login-title">Login in</h2>
+      </div>
+    </div>
 
   </div>
 </template>
-<style scoped lang="scss">
-  
 
+<style scoped>
+.login-warp {
+  min-height: 100vh;
+  display: flex;
 
-  .login-warp {
-    height: 100vh;
-    display: flex;
-    background: white;
+  /* ========== 左侧 ========== */
+  .login-left {
+    width: 60%;
+    height: 100%;
+    background: #d4cfcf;
   }
+
+  /* ========== 右侧 ========== */
+  .login-right{
+    width: 40%;
+    height: 100%;
+    background: #868080;
+  }
+}
 </style>
